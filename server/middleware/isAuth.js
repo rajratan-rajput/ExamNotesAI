41630c822a7c1b98a21da27ce018e6e3866ba031
rajratan-rajput <rraj30452@gmail.com>
2026-02-18 19:49:44 +0530

    Fix JWT auth middleware to use Authorization header

--- server/middleware/isAuth.js
+++ server/middleware/isAuth.js
@@ -1,20 +1,49 @@
-import jwt from "jsonwebtoken"
+// import jwt from "jsonwebtoken"
 
-const isAuth = async (req,res,next) => {
-    try {
-        let {token} = req.cookies
-        if(!token){
-            return res.status(400).json({message:"Token is not found"})
-        }
-        let verifyToken = jwt.verify(token ,process.env.JWT_SECRET )
-        if(!verifyToken){
-            return res.status(400).json({message:"user doesn't have valid token"})
-        }
-        req.userId = verifyToken.userId
-        next()
+// const isAuth = async (req,res,next) => {
+//     try {
+//         let {token} = req.cookies
+//         if(!token){
+//             return res.status(400).json({message:"Token is not found"})
+//         }
+//         let verifyToken = jwt.verify(token ,process.env.JWT_SECRET )
+//         if(!verifyToken){
+//             return res.status(400).json({message:"user doesn't have valid token"})
+//         }
+//         req.userId = verifyToken.userId
+//         next()
 
-    } catch (error) {
-        return res.status(500).json({message:`is auth error ${error}`})
+//     } catch (error) {
+//         return res.status(500).json({message:`is auth error ${error}`})
+//     }
+// }
+// export default isAuth
+
+import jwt from "jsonwebtoken";
+
+const isAuth = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(400).json({ message: "Token is not found" });
     }
-}
-export default isAuth
+
+    const token = authHeader.split(" ")[1];
+
+    const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!verifyToken) {
+      return res.status(400).json({ message: "User doesn't have valid token" });
+    }
+
+    req.userId = verifyToken.userId;
+
+    next();
+
+  } catch (error) {
+    return res.status(401).json({ message: "Auth error", error: error.message });
+  }
+};
+
+export default isAuth;
